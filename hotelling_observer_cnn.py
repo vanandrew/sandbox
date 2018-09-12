1b1b481357d86133b18414c042c5232aa5e74277
Andrew Van <vanandrew@wustl.edu>
2018-09-12 12:56:59 -0500

    use more efficient datatypes

--- hotelling_observer_cnn.py
+++ hotelling_observer_cnn.py
@@ -6,7 +6,7 @@ import numpy as np
 tf.logging.set_verbosity(tf.logging.INFO)
 
 # settings
-train_size = 100
+train_size = 1000
 
 # load dataset
 mat_contents = sio.loadmat('dataset.mat')
@@ -22,8 +22,8 @@ signal_present = np.swapaxes(signal_present,0,1)
 # split the dataset into train,validation
 signal_absent_train = signal_absent[0:train_size,:,:]
 signal_present_train = signal_present[0:train_size,:,:]
-signal_absent_val = signal_absent[train_size:200,:,:,]
-signal_present_val = signal_present[train_size:200,:,:]
+signal_absent_val = signal_absent[train_size:1100,:,:,]
+signal_present_val = signal_present[train_size:1100,:,:]
 
 # now combine the positive/negative examples into one array
 train_set = np.concatenate((signal_absent_train,signal_present_train),axis=0)
@@ -35,21 +35,21 @@ val_set = np.reshape(val_set,(-1,64,64,1))
 
 # create labels for training and validation set
 labels_train = tf.concat([
-        tf.zeros(signal_absent_train.shape[0],dtype=tf.float64),
-        tf.ones(signal_present_train.shape[0],dtype=tf.float64)
+        tf.zeros(signal_absent_train.shape[0],dtype=tf.float32),
+        tf.ones(signal_present_train.shape[0],dtype=tf.float32)
     ],axis=0)
 labels_val = tf.concat([
-        tf.zeros(signal_absent_val.shape[0],dtype=tf.float64),
-        tf.ones(signal_present_val.shape[0],dtype=tf.float64)
+        tf.zeros(signal_absent_val.shape[0],dtype=tf.float32),
+        tf.ones(signal_present_val.shape[0],dtype=tf.float32)
     ],axis=0)
 
 # setup placeholder for network input
-net_input = tf.placeholder(tf.float64,shape=[None,64,64,1])
+net_input = tf.placeholder(tf.float32,shape=[None,64,64,1])
 
 # setup layers
 conv0 = tf.layers.conv2d(inputs=net_input,filters=32,kernel_size=(5,5),padding='same',activation=tf.nn.relu,name='conv0')
 pool0 = tf.layers.max_pooling2d(inputs=conv0,pool_size=(2,2),strides=2,name='pool0')
-dense0 = tf.layers.dense(inputs=tf.reshape(pool0,[-1,pool0.shape[1]*pool0.shape[2]*32]),units=128,activation=tf.nn.relu,name='dense0')
+dense0 = tf.layers.dense(inputs=tf.reshape(pool0,[-1,pool0.shape[1]*pool0.shape[2]*32]),units=1024,activation=tf.nn.relu,name='dense0')
 readout = tf.squeeze(tf.layers.dense(inputs=dense0,units=1))
 
 # set loss function
@@ -71,7 +71,11 @@ AUC_summary = tf.summary.scalar('AUC',AUC)
 summary_op = tf.summary.merge([loss_summary,train_summary])
 
 # setup and run tensorflow session
-with tf.Session() as sess:
+session_conf = tf.ConfigProto(
+        intra_op_parallelism_threads=6,
+        inter_op_parallelism_threads=6
+    )
+with tf.Session(config=session_conf) as sess:
     # initialize all variables
     sess.run([tf.global_variables_initializer(),tf.local_variables_initializer()])
     writer = tf.summary.FileWriter('./logdir',sess.graph)
