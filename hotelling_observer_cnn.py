e466ba3a8bb6f42277e697f429a48f08da914c51
Andrew Van <vanandrew@wustl.edu>
2018-09-12 23:57:19 -0500

    working cnn maybe?

--- hotelling_observer_cnn.py
+++ hotelling_observer_cnn.py
@@ -1,82 +1,116 @@
 #!/usr/bin/env python3
+# pylint: disable=R0914
+"""
+A CNN emulating a hotelling observer
+"""
 
+import shutil
 import tensorflow as tf
-import scipy.io as sio
 import numpy as np
+from lumpybg import data_import
 tf.logging.set_verbosity(tf.logging.INFO)
 
-# settings
-train_size = 1000
+def get_batch(n_size, data, labels):
+    """
+    Return n-size batch of data and labels respectively
+    """
+    # data and labels must be same length in 1st dim
+    assert len(data) == len(labels), "Data and Labels not the same length!"
 
-# load dataset
-mat_contents = sio.loadmat('dataset.mat')
-signal_absent = mat_contents['signal_absent']
-signal_present = mat_contents['signal_present']
+    # get indicies for shuffle
+    idx = np.arange(len(data))
+    np.random.shuffle(idx)
 
-# format data for tensorflow
-signal_absent = np.swapaxes(signal_absent,1,2)
-signal_absent = np.swapaxes(signal_absent,0,1)
-signal_present = np.swapaxes(signal_present,1,2)
-signal_present = np.swapaxes(signal_present,0,1)
+    # return batch
+    return data[idx[0:n_size]], labels[idx[0:n_size]]
 
-# split the dataset into train,validation
-signal_absent_train = signal_absent[0:train_size,:,:]
-signal_present_train = signal_present[0:train_size,:,:]
-signal_absent_val = signal_absent[train_size:1100,:,:,]
-signal_present_val = signal_present[train_size:1100,:,:]
+def main():
+    """
+    Main Function
+    """
 
-# now combine the positive/negative examples into one array
-train_set = np.concatenate((signal_absent_train,signal_present_train),axis=0)
-val_set = np.concatenate((signal_absent_val,signal_present_val),axis=0)
+    # get the lumpy background data
+    train_set, val_set, train_label, val_label = data_import('dataset.mat', 9900)
 
-# now reshape for features layer
-train_set = np.reshape(train_set,(-1,64,64,1))
-val_set = np.reshape(val_set,(-1,64,64,1))
+    # setup placeholder for network input
+    net_input = tf.placeholder(tf.float32, shape=[None, 64, 64, 1])
 
-# create labels for training and validation set
-labels_train = tf.concat([
-        tf.zeros(signal_absent_train.shape[0],dtype=tf.float32),
-        tf.ones(signal_present_train.shape[0],dtype=tf.float32)
-    ],axis=0)
-labels_val = tf.concat([
-        tf.zeros(signal_absent_val.shape[0],dtype=tf.float32),
-        tf.ones(signal_present_val.shape[0],dtype=tf.float32)
-    ],axis=0)
+    # setup layers
+    conv0 = tf.layers.conv2d(
+        inputs=net_input,
+        filters=32,
+        kernel_size=(5, 5),
+        padding='same',
+        activation=tf.nn.relu,
+        name='conv0')
+    pool0 = tf.layers.max_pooling2d(inputs=conv0, pool_size=(2, 2), strides=2, name='pool0')
+    dense0 = tf.layers.dense(
+        inputs=tf.reshape(pool0, [-1, int(pool0.shape[1]*pool0.shape[2]*32)]),
+        units=1024, activation=tf.nn.relu,
+        name='dense0')
+    readout = tf.squeeze(tf.layers.dense(inputs=dense0, units=1))
 
-# setup placeholder for network input
-net_input = tf.placeholder(tf.float32,shape=[None,64,64,1])
+    # create a placeholder to feed in data for loss function
+    label_cmp = tf.placeholder(tf.float32)
 
-# setup layers
-conv0 = tf.layers.conv2d(inputs=net_input,filters=32,kernel_size=(5,5),padding='same',activation=tf.nn.relu,name='conv0')
-pool0 = tf.layers.max_pooling2d(inputs=conv0,pool_size=(2,2),strides=2,name='pool0')
-dense0 = tf.layers.dense(inputs=tf.reshape(pool0,[-1,int(pool0.shape[1]*pool0.shape[2]*32)]),units=1024,activation=tf.nn.relu,name='dense0')
-readout = tf.squeeze(tf.layers.dense(inputs=dense0,units=1))
+    # set loss function
+    loss = tf.losses.sigmoid_cross_entropy(label_cmp, readout)
+    loss_summary = tf.summary.scalar('loss', loss)
 
-# set loss function
-loss = tf.losses.sigmoid_cross_entropy(readout,labels_train)
-loss_summary = tf.summary.scalar('loss',loss)
+    # setup optimizer
+    train_op = tf.train.AdamOptimizer().minimize(loss)
 
-# setup optimizer
-train_op = tf.train.AdamOptimizer().minimize(loss)
+    # check accuracy on training set
+    training_accuracy, ta_op = tf.metrics.accuracy(label_cmp, tf.sigmoid(readout))
+    train_summary = tf.summary.scalar('training_accuracy', training_accuracy)
 
-# check accuracy on training set
-training_accuracy,ta_op = tf.metrics.accuracy(labels_train,tf.sigmoid(readout))
-train_summary = tf.summary.scalar('training_accuracy',training_accuracy)
+    # check accuracy on validation set
+    validation_accuracy, val_op = tf.metrics.accuracy(val_label, tf.sigmoid(readout))
+    validation_summary = tf.summary.scalar('validation_accuracy', validation_accuracy)
 
-# check AUC on validation set
-AUC,AUC_op = tf.metrics.auc(labels_val,tf.sigmoid(readout))
-AUC_summary = tf.summary.scalar('AUC',AUC)
+    # check AUC on validation set
+    auc, auc_op = tf.metrics.auc(val_label, tf.sigmoid(readout))
+    auc_summary = tf.summary.scalar('AUC', auc)
 
-# create summary op
-summary_op = tf.summary.merge([loss_summary,train_summary])
+    # create summary op
+    summary_op = tf.summary.merge([loss_summary, train_summary])
+    val_summary_op = tf.summary.merge([validation_summary, auc_summary])
 
-# setup and run tensorflow session
-with tf.Session() as sess:
-    # initialize all variables
-    sess.run([tf.global_variables_initializer(),tf.local_variables_initializer()])
-    writer = tf.summary.FileWriter('./logdir',sess.graph)
-    for i in range(1000):
-        summary,_,_ = sess.run([summary_op, ta_op, train_op], feed_dict={net_input: train_set})
-        writer.add_summary(summary,i)
-        AUC_summary_out,_ = sess.run([AUC_summary, AUC_op], feed_dict={net_input: val_set})
-        writer.add_summary(AUC_summary_out,i)
+    # setup and run tensorflow session
+    with tf.Session() as sess:
+        # initialize all variables
+        sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
+
+        # setup writer for tensorbaord log files
+        shutil.rmtree('./logdir', ignore_errors=True) # remove any existing log dirs
+        writer = tf.summary.FileWriter('./logdir', sess.graph)
+
+        # run epochs
+        for epoch in range(100):
+            # get mini batch for training
+            tset, lset = get_batch(32, train_set, train_label)
+
+            # train on batch
+            summary, training_accuracy_value, _, train_loss = sess.run(
+                [summary_op, ta_op, train_op, loss],
+                feed_dict={net_input: tset, label_cmp: lset})
+            writer.add_summary(summary, epoch)
+
+            # get auc on validation set
+            val_summary_out, auc_val, validation_accuracy_value, val_loss = sess.run(
+                [val_summary_op, auc_op, val_op, loss],
+                feed_dict={net_input: val_set, label_cmp: val_label})
+            writer.add_summary(val_summary_out, epoch)
+
+            # output logging info
+            tf.logging.info(("Epoch: {}, Training Loss: {}, Training Accuracy: {},"
+                             " Validation Loss: {}, Validation Accuracy: {}, AUC {}").format(
+                                 epoch,
+                                 train_loss,
+                                 training_accuracy_value,
+                                 val_loss,
+                                 validation_accuracy_value,
+                                 auc_val))
+
+if __name__ == '__main__':
+    main()
---
+++ lumpybg.py
@@ -0,0 +1,64 @@
+"""
+    Imports lumpy background data from .mat file
+"""
+import scipy.io as sio
+import numpy as np
+
+def data_import(matfile, train_size):
+    """
+        Imports the lumpy background data from the mat file. It
+        expects the matfile to contain the signal_absent and signal_present
+        variables, which are d1 x d2 x n arrays (where d1 and d2 are
+        dimensions in the array, and n is the number of samples).
+
+        The train_size parameter sets the boundary for dividing the data
+        into a training and validation set. Setting train_size to m, will
+        use m examples (from present/absent each) for the training set, and
+        the rest for validation.
+
+        Data is reshaped into (batch_size,image_dimension_1,image_dimension_2,1)
+
+        This function also creates the labels for each set, where 1 is equal to
+        signal present and 0 is equal to signal absent.
+
+        This function returns 4 outputs: training_set, validation_set,
+        training_labels, and validation_labels respectively as numpy arrays.
+    """
+
+    # load dataset
+    mat_contents = sio.loadmat(matfile)
+    signal_absent = mat_contents['signal_absent']
+    signal_present = mat_contents['signal_present']
+
+    # format data for tensorflow
+    signal_absent = np.swapaxes(signal_absent, 1, 2)
+    signal_absent = np.swapaxes(signal_absent, 0, 1)
+    signal_present = np.swapaxes(signal_present, 1, 2)
+    signal_present = np.swapaxes(signal_present, 0, 1)
+
+    # split the dataset into train,validation
+    signal_absent_train = signal_absent[0:train_size, :, :]
+    signal_present_train = signal_present[0:train_size, :, :]
+    signal_absent_val = signal_absent[train_size:, :, :]
+    signal_present_val = signal_present[train_size:, :, :]
+
+    # now combine the positive/negative examples into one array
+    train_set = np.concatenate((signal_absent_train, signal_present_train), axis=0)
+    val_set = np.concatenate((signal_absent_val, signal_present_val), axis=0)
+
+    # now reshape for features layer
+    train_set = np.reshape(train_set, (-1, 64, 64, 1))
+    val_set = np.reshape(val_set, (-1, 64, 64, 1))
+
+    # create labels for training and validation set
+    labels_train = np.concatenate([
+        np.zeros(signal_absent_train.shape[0]),
+        np.ones(signal_present_train.shape[0])
+    ], axis=0)
+    labels_val = np.concatenate([
+        np.zeros(signal_absent_val.shape[0]),
+        np.ones(signal_present_val.shape[0])
+    ], axis=0)
+
+    # return sets
+    return train_set, val_set, labels_train, labels_val
