25922e374734e258772177e28b5a0afa2f6c1134
Andrew Van <vanandrew@wustl.edu>
2018-11-02 11:34:05 -0500

    mcmc update

---
+++ mcmc_lumpy.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# pylint: disable=R0914,C0103,R0915,C0301
+"""
+    mcmc lumpy
+"""
+import numpy as np
+import numpy.random as npr
+import matplotlib.pyplot as plt
+
+class markov_chain:
+    """
+        The markov chain for storing lumpy background representations
+    """
+    def __init__(self):
+        self._chain = [] # Create a list to store elements to chain
+
+    def add(self, element):
+        """ adds element to the chain """
+        self._chain.append(element)
+
+    def get(self):
+        """ returns the entire chain """
+        return self._chain
+
+def create_lumpy_background(dim, Nbar, DC, magnitude, stddev):
+    """
+        Creates a lumpy background
+    """
+
+    # initialize image
+    b = DC*np.ones((dim, dim))
+
+    # N is the number of lumps
+    N = npr.poisson(Nbar)
+
+    # Create lumpy background image
+    for _ in range(N):
+        pos = npr.rand(2, 1)*dim
+        X, Y = np.meshgrid(
+            [i - pos[0] for i in range(dim)],
+            [i - pos[1] for i in range(dim)])
+        lmp = magnitude*np.exp(-0.5*(X**2+Y**2)/stddev**2)
+        b = b + lmp
+    return b, N
+
+def proposal_density():
+    """
+        Returns a proposal from proposal_density
+    """
+
+    return
+
+def calculate_posterior():
+    """
+        Calculates the posterior probability
+    """
+
+    return
+
+def acceptance():
+    """
+        Returns the theta using the acceptance/rejection decision
+    """
+
+    return
