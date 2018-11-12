5294e7987db8c990da6ba5117ea77ab3db8c5ad7
Andrew Van <vanandrew@wustl.edu>
2018-11-12 13:13:59 -0600

    update

--- mcmc_lumpy.py
+++ mcmc_lumpy.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
-# pylint: disable=R0914,C0103,R0915,C0301,W0611,W0102,R0913
+# pylint: disable=R0914,C0103,R0915,C0301,W0611,W0102,R0913,R0902
 """
     mcmc lumpy
 """
-from copy import deepcopy
+import pickle
 import numpy as np
 import numpy.random as npr
 import scipy.stats as ss
-import matplotlib.pyplot as plt
 
 class phi_matrix:
     """
         a representation of theta
     """
-    def __init__(self, centers, stddev=10, Nstar=10, dim=64):
+    def __init__(self, centers, stddev=10, var_noise=0.1, dim=64, Nbar=10):
+        # Save dim
+        self._dim = dim
+
+        # Save var noise
+        self._var_noise = var_noise
+
+        # Save Nbar
+        self._Nbar = Nbar
+
         # create list to store theta
         self._theta = []
+
+        # Set Nstar
+        Nstar = len(centers)
 
         # set the N'
         self._Nprime = Nstar*100
@@ -28,6 +39,9 @@ class phi_matrix:
 
         # create phi matrix
         self._phi = np.zeros((self._Nprime, 3))
+
+        # create gaussian noise
+        self._noise = npr.normal(0, var_noise**0.5, (dim, dim))
 
         # assign centers to phi matrix
         for n, pos in enumerate(centers):
@@ -42,26 +56,28 @@ class phi_matrix:
                 self._phi[n, 1:3] = npr.rand(2)*dim
 
         # Save the current phi_matrix in theta
-        self._theta.append(deepcopy(self._phi))
+        self._theta.append(np.copy(self._phi))
+
+        # Save g into a variable
+        self._g, _, _ = create_lumpy_background(pos=self._phi)
 
     def shift_centers(self):
         """
             shifts center of a single alpha element = 1
         """
-        # get only realized lumps
-        pos = [center[1:3] for center in self._phi if center[0] == 1]
+        # get realized lumps from previous realization
+        pos = [center[1:3] for center in self._theta[-1] if center[0] == 1]
 
         # get the number of lumps set
         N = len(pos)
 
         # randomly choose a lump to shift
         choice = npr.choice(N)
-        print(choice)
 
         # shift the correct lump
         i = 0
         for n, _ in enumerate(self._phi):
-            if self._phi[n, 0] == 1:
+            if self._phi[n, 0] == 1 and self._theta[-1][n, 0] == 1:
                 if i == choice:
                     self._phi[n, 1] = self._phi[n, 1] + npr.randn()*self._gsd
                     self._phi[n, 2] = self._phi[n, 2] + npr.randn()*self._gsd
@@ -84,10 +100,13 @@ class phi_matrix:
         self.flip_alpha()
         self.shift_centers()
 
-    def grab_chain(self):
+    def grab_chain(self, real=True):
         """
             returns the markov chain
         """
+        if real: # only return entries where alpha = 1
+            return [np.array([row for row in theta if row[0] == 1]) for theta in self._theta]
+        # return everything
         return self._theta
 
     def acceptance(self):
@@ -97,37 +116,37 @@ class phi_matrix:
         # calculate posterior ratio and calculate probability acceptance
         p1a, p2a = self._calculate_posterior_components(theta=self._phi)
         p1b, p2b = self._calculate_posterior_components(theta=self._theta[-1])
+        # print(np.prod(p1a/p1b))
+        # print(p2a/p2b)
         posterior_ratio = np.prod(p1a/p1b)*(p2a/p2b)
-        print(posterior_ratio)
+        # print(posterior_ratio)
         prob_accept = np.minimum(1, posterior_ratio)
-        print(prob_accept)
+        # print(prob_accept)
 
         # add new if within probability
         if npr.rand() < prob_accept:
+            # print('added new')
             self._add_new()
         # add previous if not within probability
         else:
+            # print('added last')
             self._add_last()
             # reset phi to last theta
-            self._phi = self._theta[-1]
+            self._phi = np.copy(self._theta[-1])
 
-    @staticmethod
-    def _calculate_posterior_components(theta, var_noise=0.1, dim=64, Nbar=10):
+    def _calculate_posterior_components(self, theta):
         """
             Calculates the posterior probability
         """
-        # generate the mumpy background using theta
-        b, N, pos = create_lumpy_background(pos=theta)
-        print(pos)
+        # Get number of lumps
+        b, N, _ = create_lumpy_background(pos=theta)
 
-        # add gaussian noise
-        gH0 = b + npr.normal(0, var_noise**0.5, (dim, dim))
+        s = b + self._noise
 
-        # calculate the probabiltiy g given b
-        prgbH0 = ss.norm(0, var_noise**0.5).pdf(gH0 - b).ravel()
+        prgbH0 = ss.norm(s, self._var_noise**0.5).pdf(self._g).ravel()
 
         # calculate pr(N)*pr({c_n})
-        prNprcn = np.exp(-Nbar)*(Nbar/(dim*dim))**N
+        prNprcn = np.exp(-self._Nbar)*(self._Nbar/(self._dim*self._dim))**N
 
         # return components
         return prgbH0, prNprcn
@@ -136,13 +155,13 @@ class phi_matrix:
         """
             adds the proposed phi to the markov chain
         """
-        self._theta.append(deepcopy(self._phi))
+        self._theta.append(np.copy(self._phi))
 
     def _add_last(self):
         """
             adds the last phi to the chain
         """
-        self._theta.append(self._theta[-1])
+        self._theta.append(np.copy(self._theta[-1]))
 
 def create_lumpy_background(Nbar=10, DC=20, magnitude=1, stddev=10, dim=64, pos=[]):
     """
@@ -189,14 +208,15 @@ def main():
     """
     _, _, pos = create_lumpy_background()
     phi = phi_matrix(centers=pos)
-    for i in range(5000):
-        print("")
+    for i in range(150000):
         print(i)
+        # calculate the probabiltiy g given b
         phi.flip_and_shift()
         phi.acceptance()
-    #print(phi.grab_chain())
+        # breakpoint()
 
-
+    with open('work.pkl', 'wb') as f:
+        pickle.dump(phi, f)
 
 if __name__ == "__main__":
     main()
